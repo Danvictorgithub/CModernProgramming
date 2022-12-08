9699ff7c9db8c9594182b6a5cb1b9938694a18af
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2022-12-08 11:21:20 +0800

    Added SelectionSort

--- My-DSA/selectionSort.c
+++ My-DSA/selectionSort.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h> //malloc
-void selectionSort(int *array, int arraySize) {
+int *selectionSort(int *array, int arraySize) {
 	int *sortedArray = malloc(sizeof(int)*arraySize);
-	int min;
-	int i;
+	int min = 0, minIndex;
+	int i,j;
+	int currentArraySize = arraySize;
 	for (i = 0; i < arraySize;i++) {
-
+		min = array[0];
+		for (j = 0; j < currentArraySize;j++) {
+			if (min >= array[j]) {
+				min = array[j];
+				minIndex = j;
+			}
+		}
+		sortedArray[i] = min;
+		currentArraySize--;
+		for (j = minIndex; j < currentArraySize;j++) {
+			array[j] = array[j+1];
+		}
 	}
+	return sortedArray;
 }
 
 void printArray(int *array, int arraySize) {
@@ -18,9 +31,9 @@ void printArray(int *array, int arraySize) {
 
 int main(void) {
 	int array[5] = { 3,4,2,1,7};
-	// printArray(array, sizeof(array)/sizeof(array[0]));
-	// selectionSort(array,sizeof(array)/sizeof(array[0]));
-	// printArray(array, sizeof(array)/sizeof(array[0]));
+	printArray(array, sizeof(array)/sizeof(array[0]));
+	int *newArray = selectionSort(array,sizeof(array)/sizeof(array[0]));
+	printArray(newArray, sizeof(array)/sizeof(array[0]));
 	return 0;
 
 }
