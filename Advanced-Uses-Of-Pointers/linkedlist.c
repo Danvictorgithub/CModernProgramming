e61cab794583bbb276f5a81fc5d404816434a7a2
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2022-12-13 09:02:27 +0800

    Added linkedlist search

--- Advanced-Uses-Of-Pointers/linkedlist.c
+++ Advanced-Uses-Of-Pointers/linkedlist.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+// Linked_List Initialization
 struct node {
 	int value;
 	struct node *next;
 };
-
+// Creating a List as a Data Structure
 struct node *add_to_list(struct node *list, int n) {
 	struct node *new_node;
 	new_node = malloc(sizeof(struct node));
@@ -17,7 +17,7 @@ struct node *add_to_list(struct node *list, int n) {
 	new_node->next = list;
 	return new_node;
 }
-
+// Put values on list
 struct node *read_numbers(void) {
 	struct node *first = NULL;
 	int n;
@@ -30,17 +30,60 @@ struct node *read_numbers(void) {
 		first = add_to_list(first, n);
 	}
 }
-
-void printLinkedList(struct node *NODE) {
+// Prints List Recursive Method
+void printLinkedListRec(struct node *NODE) {
 	printf("%d",NODE->value);
 	if (NODE->next != NULL)
-		printLinkedList(NODE->next);
-} 
+		printLinkedListRec(NODE->next);
+}
+// Prints List Iterative Method
+void printLinkedList(struct node *NODE) {
+	struct node *p;
+	for (p = NODE; p != NULL;p = p->next) {
+		printf("%d",p->value);
+	}
+}
+// struct node *search_list(struct node *list, int n) {
+// 	struct node *p;
 
+// 	for (p = list; p != NULL; p =  p->next) {
+// 		if (p->value == n)
+// 			return p;
+// 	}
+// 	return NULL;
+// }
+
+// struct node *search_list(struct node *list,int n) {
+// 	for (; list!= NULL; list = list->next){
+// 		if (list->value == n) {
+// 			return list;
+// 		}
+// 	}
+// 	return NULL;
+// }
+
+// struct node *search_list(struct node *list, int n) {
+// 	for (;list != NULL && list->value != n; list = list->next);
+// 		return list;
+// }
+
+struct node *search_list(struct node *list, int n) {
+	while (list != NULL && list->value != n) {
+		list = list->next;
+	}
+	return list;
+}
 int main(void) {
 	struct node *first;
 	first = read_numbers();
 	printf("PRINTING\n");
-	printLinkedList(first);
+	// printLinkedList(first);
+	// for (struct node *p = first; p != NULL; p = p->next) {
+	// 	printf("%d ",p->value);
+	// }
+	int numSearch; printf("Enter Number to Search: "),scanf("%d",&numSearch);
+	
+	struct node *result_node = search_list(first, numSearch);
+	(result_node != NULL) ? printf("Result Found: %d",result_node->value): printf("Result Does Not Exist");
 	return 0;
 }
