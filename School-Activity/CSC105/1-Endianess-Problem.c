e0afc68635dde7e815b7ab89930c52c5debd2263
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2023-08-30 13:54:21 +0800

    Edited solution on CSC105 Endinaess Problem

--- School-Activity/CSC105/1-Endianess-Problem.c
+++ School-Activity/CSC105/1-Endianess-Problem.c
@@ -3,10 +3,12 @@
 void printLittleEndian(int n) {
     int tempArray[10] = {0};
     int i = 0;
+    // Gets Individual Digit and stored in Array in reversed order
     for ( ; n != 0 ; i++) {
         tempArray[i] = n % 10;
         n /= 10;
     }
+    // Prints the Array in reversed order
     printf("Little Endian: ");
     for (; i > 0 ;printf("%d ",tempArray[--i]));
     printf("\n");
@@ -14,11 +16,13 @@ void printLittleEndian(int n) {
 void printBigEndian(int n) {
    int tempArray[10] = {0};
     int i = 0;
+    // Gets Individual Digit and stored in Array in reversed order
     for ( ; n != 0 ; i++) {
         tempArray[i] = n % 10;
         n /= 10;
     }
-    printf("Big Endian: ");
+    // Prints Array in normal order
+    printf("Big Endian:    ");
     for (int j = 0 ; j != i ;printf("%d ",tempArray[j++]));
     printf("\n"); 
 }
@@ -26,6 +30,7 @@ int main(void) {
     int value = 0;
     printf("Enter any numeric value: ");
     scanf("%d", &value);
-    (value != 0) ? printLittleEndian(value),printBigEndian(value):printf("Little Endian: 0\nBig Endian: 0\n");
+    // Added Edge case if the input value is 0 to avoid Segmentation Fault
+    (value != 0) ? printBigEndian(value),printLittleEndian(value):printf("Little Endian: 0\nBig Endian: 0\n");
     return 0;
 }
